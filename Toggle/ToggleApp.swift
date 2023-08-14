123873f76fe515a77fe8d5dfe383ce1a3f7a1cf2
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-08-14 09:32:58 +0500

    Initial Commit

---
+++ Toggle/ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleApp.swift
+//  Toggle
+//
+//  Created by Bilal Ahmed on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
